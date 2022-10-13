3a5ce6800fac345f6d17f78ca65b4a84302b6031
Sheer Curiosity <ryanseverinm@gmail.com>
2022-10-12 22:49:00 -0700

    removed clean_dist.py devscript

--- devscripts/clean_dist.py
+++
@@ -1,9 +0,0 @@
-import os
-
-buildFiles = [f for f in os.listdir('./') if os.path.isfile(os.path.join('./', f))]
-
-for file in buildFiles:
-    if file.endswith('.spec'):
-        os.remove(f"./{file}")
-    else:
-        continue
